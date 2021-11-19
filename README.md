30ba81439fa85746c206b21d35f205e451475ee7
Mukadam Samod <samodmukadam2005@gmail.com>
2021-11-19 04:37:45 -0800

    Add text to web-page

---
+++ README.md
@@ -0,0 +1,44 @@
+# Application Details
+### Application Author: [
+    Name: Samod Mukadam Oluwatobi
+    Github: samodmukadam21
+    Phonenumber: 08129583518
+    Email: samodmukadam2005@gmail.com
+    WhatsApp Number: 08129583518
+]
+
+
+### Application Technology: [
+    HTML
+    CSS
+    Bootstrap
+    Javascript
+    jQuery
+    Javascript
+]
+
+### Application Description
+*This application is a Calculator*
+
+### Aplication Git Branch :[
+    main (default branch)   
+]
+
+### Aplication Challenge :
+1. I had challenges when trying to let number to add.
+2. Rules from my Jquery were not affecting my HTML page.
+
+
+### Application Github Information : [
+    Repository Name Arithmetic-calculator
+    Remove URL : 
+    samodmukadam21.git/Arithmetic-calculator
+]
+
+### Application how to calculate :
+1. Write a number in Number1 and Number2 press add,sub,div,mul,mod
+2. add means to Add your number.
+3. sub means to subtract your nmber.
+4. div means to Divide your number.
+5. mul means to multiply your number.
+6. mod means to find the remaider when one integer is divided by another.
---
+++ js/script.js
@@ -0,0 +1,107 @@
+function add(num1, num2){
+  let result = num1+num2;
+  return result;
+
+}
+
+function sub(num1, num2){
+  let result = num1-num2;
+  return result;
+
+}
+
+function mul(num1, num2){
+  let result = num1*num2;
+  return result;
+
+}
+
+function div(num1, num2){
+  let result = num1/num2;
+  return result;
+
+}
+
+function modulo(num1, num2){
+  let result = num1%num2;
+  return result;
+
+}
+
+$(document).ready(function(){
+ $(".add").click(function(event){
+  event.preventDefault();
+  let input1= $("#number1").val();
+  let input2= $("#number2").val();
+
+  $("#number1").text();
+  $("#number2").text();
+
+  let nomm1= Number(input1);
+  let nomm2= Number(input2);
+
+  let answer= add(nomm1,nomm2)
+
+  $("#total").text(answer);
+});
+
+$(".sub").click(function(event){
+  event.preventDefault();
+  let input1= $("#number1").val();
+  let input2= $("#number2").val();
+
+  $("#number1").text();
+  $("#number2").text();
+
+  let nomm1= Number(input1);
+  let nomm2= Number(input2);
+
+  let answer= sub(nomm1,nomm2)
+
+  $("#total").text(answer);
+});
+
+$(".div").click(function(event){
+  event.preventDefault();
+  let input1= $("#number1").val();
+  let input2= $("#number2").val();
+
+  $("#number1").text();
+  $("#number2").text();
+
+  let nomm1= Number(input1);
+  let nomm2= Number(input2);
+
+  let answer= div(nomm1,nomm2)
+
+  $("#total").text(answer);
+});
+
+$(".mul").click(function(event){
+  event.preventDefault();
+  let input1= $("#number1").val();
+  let input2= $("#number2").val();
+
+  $("#number1").text();
+  $("#number2").text();
+
+  let nomm1= Number(input1);
+  let nomm2= Number(input2);
+
+  let answer= mul(nomm1,nomm2)
+
+  $("#total").text(answer);
+});
+
+$(".mod").click(function(event){
+  event.preventDefault();
+  let input1= parseInt($("#number1").val());
+  let input2= parseInt($("#number2").val());
+
+
+  let answer= modulo(input1,input2);
+
+  $("#total").text(answer);
+});
+
+});
